58d47c0b36e7249338da8a452e2426c8109839eb
Captiva Elias <eliasls2002@yahoo.de>
2023-07-06 03:50:20 +0200

    54:35

--- script/DeployFundMe.s.sol
+++ script/DeployFundMe.s.sol
@@ -4,11 +4,17 @@ pragma solidity ^0.8.18;
 
 import {Script} from "forge-std/Script.sol";
 import {FundMe} from "../src/FundMe.sol";
+import {HelperConfig} from "./HelperConfig.s.sol";
 
 contract DeployFundMe is Script {
+    FundMe public fundMe;
+
     function run() external {
+        HelperConfig config = new HelperConfig();
+        address ethUsdPriceFeed = config.activeNetworkConfig();
         vm.startBroadcast();
-        FundMe fundMe = new FundMe();
+        //
+        fundMe = new FundMe(ethUsdPriceFeed);
         vm.stopBroadcast();
     }
 }
--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -4,12 +4,15 @@ pragma solidity ^0.8.18;
 
 import {Test, console} from "forge-std/Test.sol";
 import {FundMe} from "../src/FundMe.sol";
+import {DeployFundMe} from "../script/DeployFundMe.s.sol";
 
 contract FundMeTest is Test {
     FundMe fundMe;
 
     function setUp() external {
-        fundMe = new FundMe();
+        DeployFundMe deployFundMe = new DeployFundMe();
+        deployFundMe.run();
+        fundMe = deployFundMe.fundMe();
     }
 
     function testMinDollarIsFive() public {
@@ -18,7 +21,7 @@ contract FundMeTest is Test {
     }
 
     function testOwnerIsMsgSender() public {
-        assertEq(fundMe.i_owner(), address(this));
+        assertEq(fundMe.i_owner(), msg.sender);
     }
 
     function testPriceFeedVersionIsAccurate() public {
